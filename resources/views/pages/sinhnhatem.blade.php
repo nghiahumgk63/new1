de5206ff65d2834a483dd2335e657cc7f125d417
nghia <nghiahumgk63>
2022-11-11 12:54:22 +0700

    '1234'

--- resources/views/pages/sinhnhatem.blade.php
+++ resources/views/pages/sinhnhatem.blade.php
@@ -3,7 +3,9 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>Tao yêu mày Hiền ơiiii</title>
+    <link rel="icon" href="imgs/hienrin.jpg" type="image/x-icon" />
+  <link rel="image" href="imgs/hienrin.jpg" type="image/x-icon" />
     <link rel="stylesheet" href="{{asset('/css/canvas.css')}}" />
     <link rel="stylesheet" href="{{asset('/css/present.css')}}" />
     <link rel="stylesheet" href="{{asset('/css/card.css')}}" />
